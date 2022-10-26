5c0317e4999c94481a31695c7ea99e7d59c399db
Sutrisno <sutrisno.stz@bsi.ac.id>
2022-10-26 10:50:28 +0700

    tambah 4 container

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,33 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('List View'),
         ),
+        body: Container(
+          margin: EdgeInsets.all(20),
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.blue,
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.red,
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.yellowAccent,
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.black87,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
